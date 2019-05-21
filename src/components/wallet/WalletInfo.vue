8502845d3982b435a8b76143815c6f30812625a3
micmac <micmac>
2019-05-21 22:25:55 +0200

    get token amount from smartcontract call

--- src/components/wallet/WalletInfo.vue
+++ src/components/wallet/WalletInfo.vue
@@ -11,18 +11,24 @@
     </form>
     balance : {{walletInfo.balance}}
     <br>
+    TBT Token : {{tokenTBTAmount}}
+    <br>
     last trx id : {{walletInfo.lastTrxId}}
     <br>
+
   </div>
   <div v-else>
     <div class="inline">
       <div class="inline">wallet : {{walletInfo.publicKey}}</div>
       <div class="inline-red" @click="enableWalletSetMode">
-        &nbsp;<v-icon name="exchange-alt"/>
+        &nbsp;
+        <v-icon name="exchange-alt"/>
       </div>
     </div>
     <br>
     balance : {{walletInfo.balance}}
+    <br>
+    TBT Token : {{tokenTBTAmount}}    
     <br>
     last trx id : {{walletInfo.lastTrxId}}
     <br>
@@ -33,6 +39,9 @@
 import axios from "axios";
 import "vue-awesome/icons/exchange-alt";
 import Icon from "vue-awesome/components/Icon";
+import nacl from "tweetnacl";
+import bs58 from "bs58";
+
 export default {
   name: "WalletInfo",
   components: {
@@ -42,21 +51,68 @@ export default {
     return {
       walletSetMode: true,
       walletInfo: {
-        publicKey: ""
-      }
+        publicKey: "",
+      },
+      tokenTBTAmount: ""
     };
   },
   methods: {
     setWallet(e) {
       e.preventDefault();
+      var keyByte = bs58.decode(
+        "YpWzyv9PPBZhHSXMcChoaxpGtbXX4vPHCNqpDFbz2vg5gPSH1Xz35ZYftgp8EvJocRp7wU4KqjDDMBLFkZv7rSB"
+      );
+
       axios
         .get("http://localhost:8383/wallet/" + this.walletInfo.publicKey)
         .then(res => (this.walletInfo = res.data))
         .catch(e => console.log(e));
+
+      axios
+        .get(
+          "http://localhost:8383/wallet/getToken/" + this.walletInfo.publicKey
+        )
+        .then(res => {
+          var signatureHex = "";
+          var trxId;
+          console.log(res.data);
+          console.log(res.data.hexaToSign);
+          trxId = res.data.transactionId;
+
+          var signature = nacl.sign.detached(
+            Buffer.from(res.data.hexaToSign, "hex"),
+            keyByte
+          );
+          var signatureBuff = new Buffer(signature);
+          signatureHex = signatureBuff.toString("hex");
+          console.log("signature : " + signatureHex);
+          axios
+            .post(
+              "http://localhost:8383/wallet/getToken/" +
+                this.walletInfo.publicKey,
+              { signature: signatureHex, id: trxId },
+              {
+                headers: {
+                  "Content-Type": "application/json"
+                }
+              }
+            )
+            .then(res2 => {
+              console.log("token amount : "+res2.data);
+              this.tokenTBTAmount = res2.data;
+            })
+            .catch(e => console.log(e));
+        })
+        .catch(e => console.log(e));
+
+
       this.walletSetMode = false;
     },
     enableWalletSetMode() {
       this.walletSetMode = true;
+      this.walletInfo.balance="";
+      this.walletInfo.lastTrxId=null;
+      this.tokenTBTAmount= "";
     }
   }
 };

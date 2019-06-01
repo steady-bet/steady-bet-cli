26a2c87b113ac3bc6a63ecebff822a8d1077bad4
Raymond LEMARRON <raymond_lemarron>
2019-06-01 18:00:33 +0200

    format code store.js

--- src/store/store.js
+++ src/store/store.js
@@ -4,25 +4,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
-        walletData: {
-            address: '',
-            csBalance: 0,
-            tokenBalance: 0
-        },
+  state: {
+    walletData: {
+      address: '',
+      csBalance: 0,
+      tokenBalance: 0
+    }
+  },
+  mutations: {
+    changeWallet (state, address) {
+      state.walletData.address = address
     },
-    mutations: {
-        changeWallet(state, address) {
-            state.walletData.address = address;  
-        },
-        updateBalance(state, csBalance) {
-            state.walletData.csBalance = csBalance;
-        },
-        updateWalletToken(state, tokenBalance) {
-            state.walletData.tokenBalance = tokenBalance;
-        }
+    updateBalance (state, csBalance) {
+      state.walletData.csBalance = csBalance
     },
-    getters: {
-        walletData: state => state.walletData
-      }
+    updateWalletToken (state, tokenBalance) {
+      state.walletData.tokenBalance = tokenBalance
+    }
+  },
+  getters: {
+    walletData: state => state.walletData
+  }
 })

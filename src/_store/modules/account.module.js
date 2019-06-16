7173578d4cf6cf2f3e08e99cb5e9c15c2ceadfeb
Raymond LEMARRON <raymond_lemarron>
2019-06-16 03:08:01 +0200

    correct import router declaration

--- src/_store/modules/account.module.js
+++ src/_store/modules/account.module.js
@@ -1,5 +1,5 @@
 import { userService } from '../../_services'
-import { router } from '../../router'
+import router from '../../router'
 
 // try to get user from local storage
 const user = JSON.parse(localStorage.getItem('user'))

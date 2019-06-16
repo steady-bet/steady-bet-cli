2e61f97e7d477f5ab1e8038bec8778317bba4f37
Raymond LEMARRON <raymond_lemarron>
2019-06-16 02:55:13 +0200

    add default header Content-Type:application/json in axios template

--- src/_services/axios.service.js
+++ src/_services/axios.service.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 export const restHttp = axios.create({
   baseURL: 'http://localhost:8383/',
-  timeout: 1000,
-  headers: { 'X-steady-bet': 'Steadybet powered by Credits!' }
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'X-steady-bet': 'Steadybet powered by Credits'
+  }
 })

f80cbd64963d97fe00c51016c36afd878dd3448f
Raymond LEMARRON <raymond_lemarron>
2020-04-02 01:42:12 +0200

    change bet history display : aggregate bet amounts by match

--- src/components/userBets/BetHistoryItem.vue
+++ src/components/userBets/BetHistoryItem.vue
@@ -1,23 +1,31 @@
 <template>
     <div>
         <div class="bet-item">
-            <strong>Match</strong> {{bet.matchName}} @ {{ bet.pk.matchDate | moment(false, "MM-DD-YYYYTHH:mm:ss") }}
+            <strong>Match</strong> {{bet.matchName}} @ {{ bet.matchDate | moment(false, "MM-DD-YYYYTHH:mm:ss") }}
             <br/>
-            <strong>Bet on</strong> {{bet.betName}} @ {{ bet.pk.betDate | moment(false, "MM-DD-YYYYTHH:mm:ss") }}
-            <br/>
-            <strong>bet amount :</strong> {{bet.betAmount | toCredits}}
+            <div id="">
+              <strong>Bet amounts :</strong>
+              <div id="bet-amounts-by-match">
+                <ul>
+                  <li v-for="betByTeam in bet.bets" :key="betByTeam.betNumber">
+                    - {{ betByTeam.betName }} = {{ betByTeam.betAmount | toCredits }}
+                  </li>
+                </ul>
+              </div>
+              <strong>TOTAL BETS = {{bet.totalBetAmount | toCredits }}</strong>
+            </div>
             <br/>
             <!-- 0=open 1=live 2=closed -->
             <strong>match status :</strong> {{statusLabel}}
             <br/>
-            <!-- 0=open 1=live 2=closed -->
+            <!-- 0=no payout 1=payout done 2=canceled -->
             <strong>payout status :</strong> {{payoutStatusLabel}}
             <br/>
             <div v-show="hasPayout">
               <strong>payout :</strong> {{bet.payout | toCredits }}
               <br/>
             </div>
-            smartcontract : <a v-bind:href="linkToSmart">{{bet.pk.smartContract}}</a>
+            smartcontract : <a v-bind:href="linkToSmart">{{bet.smartContract}}</a>
             <hr/>
         </div>
     </div>
@@ -43,7 +51,7 @@ export default {
   },
   computed: {
     linkToSmart () {
-      return (`https://monitor.credits.com/testnet/Contract/${this.bet.pk.smartContract}`)
+      return (`https://monitor.credits.com/testnet/Contract/${this.bet.smartContract}`)
     },
     payoutStatusLabel () {
       if (this.bet.payoutStatus === 0) {
@@ -77,4 +85,13 @@ export default {
 </script>
 
 <style scoped>
+#bet-amounts div {
+  text-align:center;
+}
+#bet-amounts-by-match ul, #bet-amounts-by-match li {
+    margin: 0;
+    padding: 0;
+    text-align: left;
+    text-indent: 50%;
+}
 </style>
